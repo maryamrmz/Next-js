46eb9fdb514728fd32b54c34a1645e51bff45e5b
maryam.rpour@gmail.com <maryam.rpour@gmail.com>
2022-03-28 23:25:36 +0430

    docs: set `baseUrl` and add `moduleNameMapper` to config file

--- components/event-detail/event-summary.tsx
+++ components/event-detail/event-summary.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function EventSummary({ title }: { title: string }) {
   return (
     <h2 className="flex h-64 w-full justify-center bg-emerald-500 pt-12 text-6xl font-medium text-white">
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { FC, ReactNode } from 'react';
+import { FC } from 'react';
 
 interface ButtonProps {
   link: string;
