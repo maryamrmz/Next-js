0daaabe25d9f021ca0ae319b8411766f8e5a8825
maryam.rpour@gmail.com <maryam.rpour@gmail.com>
2022-03-29 15:40:01 +0430

    add style to the header

--- components/layout/main-header.tsx
+++ components/layout/main-header.tsx
@@ -3,12 +3,12 @@ import { VFC } from 'react';
 
 const MainHeader: VFC = () => {
   return (
-    <header>
+    <header className="flex justify-between bg-slate-800 px-14 py-4 text-white">
       <div>
         <Link href="/">Next Events</Link>
       </div>
       <nav>
-        <li>
+        <li className="list-none">
           <Link href="/events">Browse All Events</Link>
         </li>
       </nav>
